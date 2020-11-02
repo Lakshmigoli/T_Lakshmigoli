706050e18cc2e760204dd4d82b0e77755ea8bb06
Lakshmigoli <73598035+Lakshmigoli@users.noreply.github.com>
2020-11-02 18:40:00 +0530

    Add files via upload

---
+++ ListDemo.java
@@ -0,0 +1,21 @@
+package com.Arraylist;
+
+import java.util.LinkedHashSet;
+
+public class ListDemo {
+	public static void main(String args[]) {
+		LinkedHashSet<Integer> al=new LinkedHashSet<Integer>();
+		al.add(34);
+		al.add(12);
+		al.add(45);
+		al.add(76);
+		al.add(16);
+		al.add(18);
+		al.add(19);
+		al.add(46);
+		al.add(98);
+		al.add(67);
+		al.add(34);
+		System.out.println(al);
+	}
+}
