4a4ff9e354e3246c5c22906d93975b3a663d7d17
Lakshmigoli <73598035+Lakshmigoli@users.noreply.github.com>
2020-11-02 18:42:18 +0530

    Add files via upload

---
+++ Treemap.java
@@ -0,0 +1,20 @@
+package com.Hashset;
+import java.util.Treemap;
+
+public class Treemap {
+public static void main(String args[]) {
+	Treemap<Integer,String> tm=new Treemap<Integer,String>();{
+	tm.put(1234,"lucky");
+		tm.put(1235,"siri");
+		tm.put(1236,"surya");
+		tm.put(1238,"suresh");
+		tm.put(1239,"msn");
+		tm.put(1234,"lucky");
+		System.out.println(tm);
+	}
+	}
+}
+	
+
+
+
