3d240994c404826205e2668e256bea9858e356b2
Lakshmigoli <73598035+Lakshmigoli@users.noreply.github.com>
2020-11-04 16:21:37 +0530

    Add files via upload

---
+++ ifelseprogram.java
@@ -0,0 +1,21 @@
+package com.control;
+
+public class ifelseprogram {
+	public static void main(String args[])
+	{
+		System.out.println("java class started");
+		boolean skills=true;
+		if(skills==false)
+		{
+			System.out.println("u will get job");
+		}
+		else
+		{
+			System.out.println(" u won't get job");
+		}
+			
+		System.out.println("java class ended");
+		
+	}
+	}
+
