58dfd96b801abf09187918d3446910821decfa1f
Lakshmigoli <73598035+Lakshmigoli@users.noreply.github.com>
2020-11-04 16:20:49 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,12 @@
+package com.control;
+
+public class forloop {
+	public static void main(String args[])
+	{
+		int i;
+		for(i=0;i<5;i++)
+		{
+			System.out.println("testing is easy");
+		}
+	}
+}
