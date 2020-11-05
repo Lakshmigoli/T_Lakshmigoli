b60ee26220419c859cb5d1a8a7cb83a5c1bffc72
Lakshmigoli <73598035+Lakshmigoli@users.noreply.github.com>
2020-11-05 17:05:14 +0530

    Add files via upload

---
+++ StringBuffer.java
@@ -0,0 +1,21 @@
+package com.Exception;
+
+public class StringBuffer {
+
+	public StringBuffer(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sc=new StringBuffer("hello");
+		sc.append("lucky");
+		System.out.println(sc);
+	}
+
+	private void append(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
