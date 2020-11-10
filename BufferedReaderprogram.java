98efbfe391add401dfd278b80fdbb16f4f97b246
Lakshmigoli <73598035+Lakshmigoli@users.noreply.github.com>
2020-11-10 18:31:52 +0530

    Add files via upload

---
+++ BufferedReaderprogram.java
@@ -0,0 +1,34 @@
+package com.Files;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class BufferedReaderprogram {
+	
+	
+	  public static void main(String[] args) {
+
+	    // Creates an array of character
+	    char[] array = new char[100];
+
+	    try {
+	      // Creates a FileReader
+	      FileReader file = new FileReader("input");
+
+	      // Creates a BufferedReader
+	      BufferedReader input = new BufferedReader(file);
+
+	      // Reads characters
+	      input.read(array);
+	      System.out.println("Data in the file: ");
+	      System.out.println(array);
+
+	      // Closes the reader
+	      input.close();
+	    }
+
+	    catch(Exception e) {
+	      e.getStackTrace();
+	    }
+	  }
+	}
+
