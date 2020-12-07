fe8eba86e0da507493a3b0a87199c1745d68612f
Naveen Kumar <Lakshmigoli6733@gmail.com>
2020-12-07 20:18:54 +0530

    motivity app

---
+++ MotivityApplication/src/com/employee/EmployeeLogin.java
@@ -0,0 +1,51 @@
+package com.employee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class EmployeeLogin extends HttpServlet {
+Connection con=null;
+PreparedStatement ps=null;
+public void init(ServletConfig config) {
+try {
+Class.forName("com.mysql.cj.jdbc.Driver");
+}catch (ClassNotFoundException e) {
+e.printStackTrace();
+}
+String url="jdbc:mysql://localhost:3306/company";
+String username="root";
+String password="Root";
+try {
+con=DriverManager.getConnection(url,username,password);
+} catch(SQLException e ) {
+e.printStackTrace();}}
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException ,IOException {
+String password=request.getParameter("password");
+String email=request.getParameter("email");
+PrintWriter pw=response.getWriter();
+String sql="select * from doctor where email=? and password=?";
+try {
+ps=con.prepareStatement(sql);
+ps.setString(1,email);
+ps.setString(2, password);
+pw.println("<html> <body> bgcolor='wheat'><center><h1>");
+ResultSet x=ps.executeQuery();
+if(x.next())
+response.sendRedirect("./employee_home.html?msg=leave accepted");
+else
+	response.sendRedirect("./employee_Login.html?msg=leave not accepted");
+
+} catch(SQLException e) {
+e.printStackTrace();
+}
+}
+}
---
+++ MotivityApplication/src/com/employee/EmployeeRegister.java
@@ -0,0 +1,55 @@
+package com.employee;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class EmployeeRegister extends HttpServlet {
+Connection con=null;
+PreparedStatement ps=null;
+public void init(ServletConfig config) {
+try {
+Class.forName("com.mysql.cj.jdbc.Driver");
+}catch (ClassNotFoundException e) {
+e.printStackTrace();}
+String url="jdbc:mysql://localhost:3306/company";
+String username="root";
+String password="Root";
+try {
+con=DriverManager.getConnection(url,username,password);
+} catch(SQLException e ) {
+e.printStackTrace();}}
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException ,IOException {
+String name=request.getParameter("name");
+String password=request.getParameter("password");
+String email=request.getParameter("email");
+String phone=request.getParameter("phone");
+long mobile=Long.parseLong(phone);
+String designation=request.getParameter("designation");
+String salary=request.getParameter("salary");
+double company=Long.parseLong(salary);
+String noofworkinghours=request.getParameter("noofworkinghours");
+PrintWriter pw=response.getWriter();
+String sql="insert into manager(name,password,email,phone,designation,salary,noofworkinghours) values(?,?,?,?,?,?,?)";
+try {
+ps=con.prepareStatement(sql);
+ps.setString(1, name);
+ps.setString(2, password);
+ps.setString(3,email);
+ps.setLong(4,mobile);
+ps.setString(5, designation);
+ps.setString(6, salary);
+ps.setString(7,noofworkinghours);
+pw.println("<html> <body bgcolor='wheat'><center><h1>");
+int x=ps.executeUpdate();
+if(x!=0)
+	response.sendRedirect("./employee_Login.html?msg=leave not accepted");
+
+} catch(SQLException e) {
+e.printStackTrace();}}}
